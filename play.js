badd5057d123954409771f94504f1dcc472b9c93
Erica-jihyeon <jihyeon20@gmail.com>
2021-12-01 22:28:43 +0000

    make a client file

--- play.js
+++ play.js
@@ -1,27 +1,3 @@
-const net = require("net");
+const { net, connect } = require('./client');
 
-const connect = function () {
-  //net.createConnection() -> initiates connection with socket.connect() & return net.Soket
-  const conn = net.createConnection({
-    host: '165.227.47.243' ,
-    port: 50541
-  });
-
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-
-  conn.on('connection', () => {
-    conn.write('Name:Erica');
-  });
-
-  //conn is the object === net.Soket
-  //return conn;
-}
-
-console.log("Connecting ...")
 connect();
-
-
