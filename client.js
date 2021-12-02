798e14b589a8200af1f72fa535c835536f8522e6
Erica-jihyeon <jihyeon20@gmail.com>
2021-12-02 00:24:13 +0000

    add move

--- client.js
+++ client.js
@@ -18,17 +18,78 @@ const connect = function () {
     conn.write('Name: ER2');
   })
 
+  conn.on('connect', () => {
+    setTimeout(() => {
+      conn.write('Move: up')
+    }, 1000);
+  })
+
   conn.on("data", (data) => {
     console.log(data);
   })
+
   //conn is the object === net.Soket
   //return conn;
+
+  /*let count = 0;
+
+  const moveL = (until) => {
+    conn.write('Move: left');
+    count++;
+    if (count > until) {
+      clearInterval(tId1)
+      count = 0;
+    }
+  }
+
+  const moveD = (until) => {
+    conn.write('Move: down');
+    count++;
+    if (count > until) {
+      clearInterval(tId2)
+      count = 0;
+    }
+  }
+
+  const moveR = (until) => {
+    conn.write('Move: right');
+    count++;
+    if (count > until) {
+      clearInterval(tId3)
+      count = 0;
+    }
+  }
+
+  const moveU = (until) => {
+    conn.write('Move: up');
+    count++;
+    if (count > until) {
+      clearInterval(tId4)
+      count = 0;
+    }
+  }
+
+  const tId1 = setInterval(moveL, 300, 5);
+  const tId2 = setInterval(moveD, 300, 5);
+  const tId3 = setInterval(moveR, 300, 5);
+  const tId4 = setInterval(moveU, 300, 5);*/
 }
 
 console.log("Connecting ...")
+
+// let count = 0;
+// const moveL = (c) => {
+//   conn.write('Move: left')
+//   c++;
+//   if (c === 5) {
+//     clearInterval(this);
+//   }
+// }
+
 
 module.exports = {
   net,
   connect
 };
+
 
